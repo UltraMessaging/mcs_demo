6258d6b170722d07abb41275de108ebfdc1445eb
Steve Ford <sford@geeky-boy.com>
2022-11-11 17:48:56 -0500

    Minor

--- tst.sh
+++ tst.sh
@@ -114,4 +114,33 @@ select * from dromonmsg;
 select * from srsmonmsg;
 __EOF__
 if [ "$?" -ne 0 ]; then echo "`date` Error, see mcs.out" >&2; exit 1; fi
-echo "`date` See mcs.out for raw data; lbmmon.log for pretty data."
+
+# Let's find some interesting records.
+
+echo "umesrc last source record:"
+egrep '"applicationId":"umesrc".*"sourceFlag":"SOURCE_NORMAL"' mcs.out | tail -1
+echo ""
+
+echo "umercv last receiver record:"
+egrep '"applicationId":"umercv".*"sourceFlag":"SOURCE_NORMAL"' mcs.out | tail -1
+echo ""
+
+echo "store last receive record:"
+egrep '"applicationId":"store_appid_topic1".*"naksSent":' mcs.out | tail -1
+echo ""
+
+echo "store last daemon stats record:"
+egrep '"applicationId":"store_topic1".*"srcRepoStats":\[{"srcRegid":' mcs.out | tail -1
+echo ""
+
+echo "DRO last proxy receiver record:"
+egrep '"applicationId":"Gateway_Portal_TRD1_rcv_ctx".*"sourceFlag":"SOURCE_NORMAL"' mcs.out | tail -1
+echo ""
+
+echo "DRO last proxy source record:"
+egrep '"applicationId":"Gateway_Portal_TRD2_src_ctx".*"sourceFlag":"SOURCE_NORMAL"' mcs.out| tail -1
+echo ""
+
+echo "DRO last daemon stats record:"
+egrep '"applicationId":"dro1".*' mcs.out| tail -1
+echo ""
